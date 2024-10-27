df32321d604c1b9bca95691cc868ba083543bf4d
MaxNoragami <maxcraftmd@gmail.com>
2024-10-28 00:02:29 +0200

    Task 2, chore: initiated the project and the 'FileReader' static class

---
+++ lab1/Task2/FileReader.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace Task2
+{
+    static class FileReader
+    {
+        public static String? ReadFileIntoString(String? path)
+        {
+            if(path != null)
+            {
+                if(File.Exists(path))
+                {
+                    return File.ReadAllText(path);
+                }
+                else
+                {
+                    Console.WriteLine("WARNING: File at {0}, couldn't be found!", path);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("WARNING: No path was provided!");
+            }
+            return null;
+        }
+
+    }
+}
---
+++ lab1/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            if(args.Length > 0)
+            {
+                FileReader.ReadFileIntoString(args[0]);
+            }
+            else
+            {
+                Console.WriteLine("ERROR: No arguments were provided!");
+            }
+
+        }
+    }
+}
