e09cf7ddd4401656c0e0932dd705b38ce9ad59ee
MaxNoragami <maxcraftmd@gmail.com>
2024-10-28 00:31:06 +0200

    Task 2, feat: implemented the core elements of the 'TextData' class

---
+++ lab1/Task2/TextData.cs
@@ -0,0 +1,67 @@
+namespace Task2
+{
+    class TextData
+    {
+        // Private attributes definition
+        private String? fileName;
+        private String? text;
+        private int numberOfVowels;
+        private int numberOfConsonants;
+        private int numberOfLetters;
+        private int numberOfSentences;
+        private String? longestWord;
+
+        // Getters
+        public String? GetFilename() {return fileName;}
+        public String? GetText() {return text;}
+        public int GetNumberOfVowels() {return numberOfVowels;}
+        public int GetNumberOfConsonants() {return numberOfConsonants;}
+        public int GetNumberOfLetters() {return numberOfLetters;}
+        public int GetNumberOfSentences() {return numberOfSentences;}
+        public String? GetLongestWord() {return longestWord;}
+
+        // Constructor
+        public TextData(String text)
+        {
+            fileName = ExtractFilename();
+            this.text = text;
+            numberOfVowels = ExtractNumberOfVowels();
+            numberOfConsonants = ExtractNumberOfConsonants();
+            numberOfLetters = ExtractNumberOfLetters();
+            numberOfSentences = ExtractNumberOfSentences();
+            longestWord = ExtractLongestWord();
+        }
+
+        // Parsing Logic
+        private string? ExtractLongestWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ExtractNumberOfSentences()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ExtractNumberOfLetters()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ExtractNumberOfConsonants()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ExtractNumberOfVowels()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string? ExtractFilename()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
