b4fd543df823685a6ac24a59d87753064c3ab813
MaxNoragami <maxcraftmd@gmail.com>
2024-10-28 09:42:54 +0200

    Task 4, feat: accepts multiple text files, outputs well defined details about the content of files

--- lab1/Task4/Program.cs
+++ lab1/Task4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            if(args.Length > 0)
+            {
+                foreach(String path in args)
+                {
+                    String? fileData = FileReader.ReadFileIntoString(path);
+                    TextData textData = new TextData(path, fileData);
+                    Console.WriteLine(textData.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR: No arguments were provided!");
+            }
+
+        }
+    }
+}
